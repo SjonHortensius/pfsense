89b65b99e9a4b0c71a73bf2a6bb55900a586b7fa
sbeaver <sbeaver@netgate.com>
2015-04-29 09:30:36 -0400

    dig_dump_states_sources.php conversion complete
    Ready for review

--- usr/local/www/diag_dump_states_sources.php
+++ usr/local/www/diag_dump_states_sources.php
@@ -29,8 +29,8 @@
 */
 
 /*
-	pfSense_BUILDER_BINARIES:	/sbin/pfctl	
-	pfSense_MODULE:	filter
+	pfSense_BUILDER_BINARIES:	/sbin/pfctl
+	pfSense_MODULE: filter
 */
 
 ##|+PRIV
@@ -56,8 +56,8 @@ if($_GET['action']) {
 }
 
 /* get our states */
-if($_GET['filter']) {
-	exec("/sbin/pfctl -s Sources | grep " . escapeshellarg(htmlspecialchars($_GET['filter'])), $sources);
+if($_POST['filter']) {
+	exec("/sbin/pfctl -s Sources | grep " . escapeshellarg(htmlspecialchars($_POST['filter'])), $sources);
 }
 else {
 	exec("/sbin/pfctl -s Sources", $sources);
@@ -67,10 +67,6 @@ $pgtitle = array(gettext("Diagnostics"),gettext("Show Source Tracking"));
 include("head.inc");
 
 ?>
-
-<body link="#0000CC" vlink="#0000CC" alink="#0000CC" onload="<?=$jsevents["body"]["onload"];?>">
-<?php include("fbegin.inc"); ?>
-<form action="diag_dump_states_sources.php" method="get" name="iform">
 
 <script type="text/javascript">
 //<![CDATA[
@@ -84,7 +80,7 @@ include("head.inc");
 		jQuery('img[name="i:' + srcip + ":" + dstip + '"]').each(busy);
 
 		jQuery.ajax(
-			"<?=$_SERVER['SCRIPT_NAME'];?>" +
+			"<?=$_SERVER['SCRIPT_NAME']?>" +
 				"?action=remove&srcip=" + srcip + "&dstip=" + dstip,
 			{ type: "get", complete: removeComplete }
 		);
@@ -93,7 +89,7 @@ include("head.inc");
 	function removeComplete(req) {
 		var values = req.responseText.split("|");
 		if(values[3] != "0") {
-			alert('<?=gettext("An error occurred.");?>');
+			alert('<?=gettext("An error occurred.")?>');
 			return;
 		}
 
@@ -104,55 +100,54 @@ include("head.inc");
 //]]>
 </script>
 
-<table width="100%" border="0" cellpadding="0" cellspacing="0">
-	<tr>
-		<td>
-		<?php
-			$tab_array = array();
-			$tab_array[] = array(gettext("States"), false, "diag_dump_states.php");
-			$tab_array[] = array(gettext("Source Tracking"), true, "diag_dump_states_sources.php");
-			$tab_array[] = array(gettext("Reset States"), false, "diag_resetstate.php");
-			display_top_tabs($tab_array);
-		?>
-		</td>
-	</tr>
-	<tr>
-		<td>
-			<div id="mainarea">
-
-<!-- Start of tab content -->
-
-<table class="tabcont" width="100%" border="0" cellspacing="0" cellpadding="0">
-	<tr>
-		<td>
-			<form action="<?=$_SERVER['SCRIPT_NAME'];?>" method="get">
-			<table class="tabcont" width="100%" border="0" cellspacing="0" cellpadding="0">
-				<tr>
-					<td>&nbsp;</td>
-					<td style="font-weight:bold;" align="right">
-						<?=gettext("Filter expression:");?>
-						<input type="text" name="filter" class="formfld search" value="<?=htmlspecialchars($_GET['filter']);?>" size="30" />
-						<input type="submit" class="formbtn" value="<?=gettext("Filter");?>" />
-					</td>
-				</tr>
-			</table>
-			</form>
-		</td>
-	</tr>
-	<tr>
-		<td>
-			<table class="tabcont sortable" width="100%" border="0" cellspacing="0" cellpadding="0">
-				<thead>
-				<tr>
-					<th class="listhdrr" width="40%"><?=gettext("Source -> Destination");?></th>
-					<th class="listhdrr" width="15%"><?=gettext("# States");?></th>
-					<th class="listhdrr" width="15%"><?=gettext("# Connections");?></th>
-					<th class="listhdr" width="15%"><?=gettext("Rate");?></th>
-					<th class="list sort_ignore" width="1%"></th>
-				</tr>
-				</thead>
-				<tbody>
 <?php
+$tab_array = array();
+$tab_array[] = array(gettext("States"), false, "diag_dump_states.php");
+$tab_array[] = array(gettext("Source Tracking"), true, "diag_dump_states_sources.php");
+$tab_array[] = array(gettext("Reset States"), false, "diag_resetstate.php");
+display_top_tabs($tab_array);
+
+require('classes/Form.class.php');
+
+$form = new Form(false);
+
+$section = new Form_Section('State/source filter');
+
+$section->addInput(new Form_Input(
+	'filter',
+	'Filter expression',
+	'text',
+	$_POST['filter'],
+	['placeholder' => '']
+));
+
+// Filter button is implemented inside the panel just because it looks neater
+// and encapsulates the filter functionality. It could be passed into Form() if preferred
+$filterbtn = new Form_Button('filterbtn', 'Filter', null);
+$filterbtn->removeClass('btn-primary')->addClass('btn-default btn-sm');
+$section->addInput(new Form_StaticText(
+	'',
+	 $filterbtn
+));
+
+$form->add($section);
+print $form;
+?>
+
+<div class="responsive">
+	<table class="table table-striped table-hover table-compact">
+		<thead>
+			<tr>
+				<th><?=gettext("Source -> Destination")?></th>
+				<th><?=gettext("# States")?></th>
+				<th><?=gettext("# Connections")?></th>
+				<th><?=gettext("Rate")?></th>
+				<th></th> <!-- For hte Remove buttin -->
+			</tr>
+		</thead>
+		<tbody>
+<?php
+
 $row = 0;
 if(count($sources) > 0) {
 	foreach($sources as $line) {
@@ -168,45 +163,34 @@ if(count($sources) > 0) {
 		$source_split = "";
 		preg_match("/(.*)\s\<?-\>?\s(.*)/", $info, $source_split);
 		list($all, $srcip, $dstip) = $source_split;
-
-		?>
-		<tr valign='top' name='r:<?php echo "{$srcip}:{$dstip}" ?>'>
-				<td class='listlr'><?php echo $info;?></td>
-				<td class='listr'><?php echo $numstates;?></td>
-				<td class='listr'><?php echo $numconnections;?></td>
-				<td class='listr'><?php echo $rate;?></td>
+?>
+			<tr name='r:<?="{$srcip}:{$dstip}" ?>'>
+				<td><?=$info?></td>
+				<td><?=$numstates?></td>
+				<td><?=$numconnections?></td>
+				<td><?=$rate?></td>
 				<td class='list'>
-				<img src='/themes/<?php echo $g['theme']; ?>/images/icons/icon_x.gif' height='17' width='17' border='0'
-					onclick="removeSource(<?php echo "'{$srcip}', '{$dstip}'"; ?>);" style='cursor:pointer;'
-					name='i:<?php echo "{$srcip}:{$dstip}"; ?>'
-					title='<?php echo gettext("Remove all source tracking entries from") . " {$srcip} " . gettext("to") . " {$dstip}";?>' alt='' />
+					<a class="btn btn-xs btn-default"
+						onclick="removeSource('<?= $srcip ?>', '<?= $dstip ?>');" name="i:<?= $srcip ?>:<?= $dstip ?>"
+						title="<?= gettext('Remove all source tracking entries from ') ?><?= $srcip ?><?= gettext(' to ') ?><?= $dstip ?>" alt="">Remove</a>
 				</td>
-			  </tr>
-		<?php
+			</tr>
+<?php
 		$row++;
 	}
 }
-else {
-	echo "<tr>
-			<td class='list' colspan='5' align='center' valign='top'>
-			  " . gettext("No source tracking entries were found.") . "
-			</td>
-		  </tr>";
-}
+
 ?>
-			</tbody>
-			</table>
-		</td>
-	</tr>
-</table>
+		</tbody>
+	</table>
+<?php
 
-<!-- End of tab content -->
+if ($row == 0) { ?>
+	<p class="alert alert-warning"><?=gettext("No source tracking entries were found.")?></p>
+<?php
+}
 
-		</div>
-	</td>
-  </tr>
-</table>
+?>
+</div>
 
-<?php require("fend.inc"); ?>
-</body>
-</html>
+<?php include("foot.inc"); ?>
