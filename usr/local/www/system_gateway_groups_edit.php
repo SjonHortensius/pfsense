e82249dbf9a6ac2e483084b22cfcce154dff82ee
sbeaver <sbeaver@netgate.com>
2015-05-27 14:55:38 -0400

    Most changes in plave
    Waiting for confirmation on Javascript operation

--- usr/local/www/system_gateway_groups_edit.php
+++ usr/local/www/system_gateway_groups_edit.php
@@ -1,23 +1,23 @@
-<?php 
+<?php
 /* $Id$ */
 /*
 	system_gateway_groups_edit.php
 	part of pfSense (https://www.pfsense.org)
-	
+
 	Copyright (C) 2010 Seth Mos <seth.mos@dds.nl>.
 	Copyright (C) 2013-2015 Electric Sheep Fencing, LP
 	All rights reserved.
-	
+
 	Redistribution and use in source and binary forms, with or without
 	modification, are permitted provided that the following conditions are met:
-	
+
 	1. Redistributions of source code must retain the above copyright notice,
 	   this list of conditions and the following disclaimer.
-	
+
 	2. Redistributions in binary form must reproduce the above copyright
 	   notice, this list of conditions and the following disclaimer in the
 	   documentation and/or other materials provided with the distribution.
-	
+
 	THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
 	INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
 	AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
@@ -30,7 +30,7 @@
 	POSSIBILITY OF SUCH DAMAGE.
 */
 /*
-	pfSense_MODULE:	routing
+	pfSense_MODULE: routing
 */
 
 ##|+PRIV
@@ -53,9 +53,9 @@ $a_gateway_groups = &$config['gateways']['gateway_group'];
 $a_gateways = return_gateways_array();
 
 $categories = array('down' => gettext("Member Down"),
-                    'downloss' => gettext("Packet Loss"),
-                    'downlatency' => gettext("High Latency"),
-                    'downlosslatency' => gettext("Packet Loss or High Latency"));
+					'downloss' => gettext("Packet Loss"),
+					'downlatency' => gettext("High Latency"),
+					'downlosslatency' => gettext("Packet Loss or High Latency"));
 
 if (is_numericint($_GET['id']))
 	$id = $_GET['id'];
@@ -83,9 +83,9 @@ if ($_POST) {
 	/* input validation */
 	$reqdfields = explode(" ", "name");
 	$reqdfieldsn = explode(",", "Name");
-	
+
 	do_input_validation($_POST, $reqdfields, $reqdfieldsn, $input_errors);
-	
+
 	if (! isset($_POST['name'])) {
 		$input_errors[] = gettext("A valid gateway group name must be specified.");
 	}
@@ -138,12 +138,12 @@ if ($_POST) {
 			$a_gateway_groups[$id] = $gateway_group;
 		else
 			$a_gateway_groups[] = $gateway_group;
-		
+
 		mark_subsystem_dirty('staticroutes');
 		mark_subsystem_dirty('gwgroup.' . $gateway_group['name']);
-		
+
 		write_config();
-		
+
 		header("Location: system_gateway_groups.php");
 		exit;
 	}
@@ -160,85 +160,235 @@ function build_gateway_protocol_map (&$a_gateways) {
 	return $result;
 }
 
+function build_carp_list() {
+	global $carplist;
+
+	$list = array('address' => gettext('Interface Address'));
+
+	foreach($carplist as $vip => $address) {
+		if(($gateway['ipprotocol'] == "inet") && (!is_ipaddrv4($address)))
+			continue;
+		if(($gateway['ipprotocol'] == "inet6") && (!is_ipaddrv6($address)))
+			continue;
+
+		$list[$vip] = "$vip - $address";
+	}
+
+	return($list);
+
+}
 include("head.inc");
 
-?>
-
-<body link="#0000CC" vlink="#0000CC" alink="#0000CC">
-
-<?php
 $gateway_protocol = build_gateway_protocol_map($a_gateways);
-$gateway_array    = array_keys($a_gateways);
-$protocol_array   = array_values($gateway_protocol);
-$protocol_array   = array_values(array_unique($gateway_protocol));
+$gateway_array	= array_keys($a_gateways);
+$protocol_array	  = array_values($gateway_protocol);
+$protocol_array	  = array_values(array_unique($gateway_protocol));
+
+if ($input_errors)
+	print_input_errors($input_errors);
+
+require('classes/Form.class.php');
+
+$form = new Form();
+
+$section = new Form_Section('Edit gateway group entry');
+
+$section->addInput(new Form_Input(
+	'nentries',
+	'Group Name',
+	'text'
+));
+
+
+$carplist = get_configured_carp_interface_list($interface);
+$row == 0;
+$numrows = count($a_gateways) - 1;
+
+foreach($a_gateways as $gwname => $gateway) {
+	if(!empty($pconfig['item'])) {
+		$af = explode("|", $pconfig['item'][0]);
+		$family = $a_gateways[$af[0]]['ipprotocol'];
+		if($gateway['ipprotocol'] != $family) {
+			$rows++;
+			continue;
+		}
+	}
+
+	$interface = $gateway['friendlyiface'];
+	$selected = array();
+
+	foreach((array)$pconfig['item'] as $item) {
+		$itemsplit = explode("|", $item);
+		if($itemsplit[0] == $gwname) {
+			$selected[$itemsplit[1]] = "selected=\"selected\"";
+			break;
+		} else {
+			$selected[0] = "selected=\"selected\"";
+		}
+	}
+
+	$group = new Form_Group($row == 0 ? 'Gateway Priority':null);
+    $group->addClass($gateway['ipprotocol']);
+    
+	$group->add(new Form_Input(
+		'gwname' . $row,
+		'Group Name',
+		'text',
+		$gateway['name']
+	))->setHelp($row == $numrows ? 'Gateway':null);
+
+	$group->add(new Form_Select(
+		$gwname,
+		'Tier',
+		!isset($pconfig['filterdescriptions']) ? '0':$pconfig['filterdescriptions'],
+		array(
+			'0' => 'Never',
+			'1' => 'Tier 1',
+			'2' => 'Tier 2',
+			'3' => 'Tier 3',
+			'4' => 'Tier 4',
+			'5' => 'Tier 5'
+		)
+	))->setHelp($row == $numrows ? 'Tier':null);
+
+	$group->add(new Form_Select(
+		$gwname . '_vip',
+		'Virtual IP',
+		!isset($pconfig['filterdescriptions']) ? '0':$pconfig['filterdescriptions'],
+		build_carp_list()
+	))->setHelp($row == $numrows ? 'Virtual IP':null);
+
+	$group->add(new Form_Input(
+		'nentries',
+		'Group Name',
+		'text',
+		$gateway['descr']
+	))->setWidth(3)->setHelp($row == $numrows ? 'Description':null);
+
+	$section->add($group);
+
+	$row++;
+}
+
+$section->addInput(new Form_StaticText(
+	'Link Priority',
+	'The priority selected here defines in what order failover and balancing of links will be done. ' .
+	'Multiple links of the same priority will balance connections until all links in the priority will be exhausted. ' .
+	'If all links in a priority level are exhausted we will use the next available link(s) in the next priority level.'
+));
+
+$section->addInput(new Form_StaticText(
+	'Virtual IP',
+	'The virtual IP field selects which (virtual) IP should be used when this group applies to a local Dynamic DNS, IPsec or OpenVPN endpoint.'
+));
+
+$section->addInput(new Form_Select(
+	'trigger',
+	'Trigger Level',
+	!isset($pconfig['filterdescriptions']) ? '0':$pconfig['filterdescriptions'],
+	array(
+		'0' => 'Member down',
+		'1' => 'Packet Loss',
+		'2' => 'High Latency',
+		'3' => 'Packet Loss or High latency'
+	)
+))->setHelp('When to trigger exclusion of a member');
+
+$section->addInput(new Form_Input(
+	'nentries',
+	'Description',
+	'text'
+))->setHelp('You may enter a description here for your reference (not parsed).');
+
+if (isset($id) && $a_gateway_groups[$id]){
+	$section->addInput(new Form_Input(
+	'id',
+	null,
+	'hidden',
+	$id
+	));
+}
+
+$form->add($section);
+
+$form->addGlobal(new Form_Button(
+	'cancel',
+	'Cancel',
+	$referer
+));
+
+print($form);
 ?>
 <script type="text/javascript">
 //<![CDATA[
-jQuery(function ($) {
-	var gateway_protocol = <?= json_encode($gateway_protocol) ?>;
-	var gateways         = <?= json_encode($gateway_array) ?>;
-	var protocols        = <?= json_encode($protocol_array) ?>;
-	if (protocols.length <= 1) { return; }
+events.push(function(){
+	jQuery(function ($) {
+		var gateway_protocol = <?= json_encode($gateway_protocol) ?>;
+		var gateways		 = <?= json_encode($gateway_array) ?>;
+		var protocols		= <?= json_encode($protocol_array) ?>;
+		if (protocols.length <= 1) { return; }
 
-	var update_gateway_visibilities = function () {
-		var which_protocol_to_show = undefined;
-		$.each(gateways, function (i, gateway) {
-			var $select = $("#" + gateway);
-			var value = $select.val();
-			var protocol = gateway_protocol[gateway];
-			if (value !== '0' /* i.e., an option is selected */) {
-				if (which_protocol_to_show === undefined) {
-					which_protocol_to_show = protocol;
-				}
-				else if (which_protocol_to_show !== protocol) {
-					which_protocol_to_show = 'ALL OF THEM'; // this shouldn't happen
-				}
-			}
-		});
-		if (which_protocol_to_show !== undefined && which_protocol_to_show !== 'ALL OF THEM') {
+		var update_gateway_visibilities = function () {
+			var which_protocol_to_show = undefined;
 			$.each(gateways, function (i, gateway) {
+				var $select = $("#" + gateway);
+				var value = $select.val();
 				var protocol = gateway_protocol[gateway];
-				var $row = $("tr.gateway_row#" + gateway + "_row");
-				if (protocol === which_protocol_to_show) {
-					if ($row.is(":hidden")) {
-						$row.fadeIn('slow');
+				if (value !== '0' /* i.e., an option is selected */) {
+					if (which_protocol_to_show === undefined) {
+						which_protocol_to_show = protocol;
 					}
-				} else {
-					if (!$row.is(":hidden")) {
-						$row.fadeOut('slow');
+					else if (which_protocol_to_show !== protocol) {
+						which_protocol_to_show = 'ALL OF THEM'; // this shouldn't happen
 					}
 				}
 			});
-		} else {
-			$("tr.gateway_row").each(function () {
-				if ($(this).is(":hidden")) {
-					$(this).fadeIn('slow');
-				}
-			});
-		}
-	};
-	$("select.gateway_tier_selector").change(update_gateway_visibilities);
-	update_gateway_visibilities();
+
+			if (which_protocol_to_show !== undefined && which_protocol_to_show !== 'ALL OF THEM') {
+				$.each(gateways, function (i, gateway) {
+					var protocol = gateway_protocol[gateway];
+					var $row = $("tr.gateway_row#" + gateway + "_row");
+					if (protocol === which_protocol_to_show) {
+						if ($row.is(":hidden")) {
+							$row.fadeIn('slow');
+						}
+					} else {
+						if (!$row.is(":hidden")) {
+							$row.fadeOut('slow');
+						}
+					}
+				});
+			} else {
+				$("tr.gateway_row").each(function () {
+					if ($(this).is(":hidden")) {
+						$(this).fadeIn('slow');
+					}
+				});
+			}
+		};
+
+		$("select.gateway_tier_selector").change(update_gateway_visibilities);
+		update_gateway_visibilities();
+	});
 });
 //]]>
 </script>
 
-<?php include("fbegin.inc"); ?>
-<?php if ($input_errors) print_input_errors($input_errors); ?>
-            <form action="system_gateway_groups_edit.php" method="post" name="iform" id="iform">
-              <table width="100%" border="0" cellpadding="6" cellspacing="0" summary="system groups edit">
+
+			<form action="system_gateway_groups_edit.php" method="post" name="iform" id="iform">
+			  <table width="100%" border="0" cellpadding="6" cellspacing="0" summary="system groups edit">
 		<tr>
 			<td colspan="2" valign="top" class="listtopic"><?=gettext("Edit gateway group entry"); ?></td>
-		</tr>	
-                <tr>
-                  <td width="22%" valign="top" class="vncellreq"><?=gettext("Group Name"); ?></td>
-                  <td width="78%" class="vtable"> 
-                    <input name="name" type="text" class="formfld unknown" id="name" size="20" value="<?=htmlspecialchars($pconfig['name']);?>" />
-                    <br /> <span class="vexpl"><?=gettext("Group Name"); ?></span></td>
-                </tr>
+		</tr>
+				<tr>
+				  <td width="22%" valign="top" class="vncellreq"><?=gettext("Group Name"); ?></td>
+				  <td width="78%" class="vtable">
+					<input name="name" type="text" class="formfld unknown" id="name" size="20" value="<?=htmlspecialchars($pconfig['name'])?>" />
+					<br /><span class="vexpl"><?=gettext("Group Name"); ?></span></td>
+				</tr>
 		<tr>
-                  <td width="22%" valign="top" class="vncellreq"><?=gettext("Gateway Priority"); ?></td>
-                  <td width="78%" class="vtable">
+				  <td width="22%" valign="top" class="vncellreq"><?=gettext("Gateway Priority"); ?></td>
+				  <td width="78%" class="vtable">
 			<table border="0" cellpadding="6" cellspacing="0" summary="gateway priority">
 			<tr>
 				<td class="listhdrr">Gateway</td>
@@ -252,6 +402,7 @@ jQuery(function ($) {
 				if(!empty($pconfig['item'])) {
 					$af = explode("|", $pconfig['item'][0]);
 					$family = $a_gateways[$af[0]]['ipprotocol'];
+
 					if($gateway['ipprotocol'] != $family)
 						continue;
 				}
@@ -266,6 +417,10 @@ jQuery(function ($) {
 						$selected[0] = "selected=\"selected\"";
 					}
 				}
+
+				print_r($gateway);
+				print('<br />');
+
 				$tr_id = $gwname . "_row";
 				echo "<tr class='gateway_row' id='{$tr_id}'>\n";
 				echo "<td class='listlr'>";
@@ -305,52 +460,50 @@ jQuery(function ($) {
 				echo "</select></td>";
 				echo "<td class='listr'><strong>{$gateway['descr']}&nbsp;</strong>";
 				echo "</td></tr>";
-		 	}
+			}
 		?>
 			</table>
 			<br /><span class="vexpl">
-			<strong><?=gettext("Link Priority"); ?></strong> <br />
+			<strong><?=gettext("Link Priority"); ?></strong><br />
 			<?=gettext("The priority selected here defines in what order failover and balancing of links will be done. " .
 			"Multiple links of the same priority will balance connections until all links in the priority will be exhausted. " .
 			"If all links in a priority level are exhausted we will use the next available link(s) in the next priority level.") ?>
 			<br />
-			<strong><?=gettext("Virtual IP"); ?></strong> <br />
+			<strong><?=gettext("Virtual IP"); ?></strong><br />
 			<?=gettext("The virtual IP field selects what (virtual) IP should be used when this group applies to a local Dynamic DNS, IPsec or OpenVPN endpoint") ?>
 			</span><br />
 		   </td>
-                </tr>
-                <tr>
-                  <td width="22%" valign="top" class="vncellreq"><?=gettext("Trigger Level"); ?></td>
-                  <td width="78%" class="vtable">
+				</tr>
+				<tr>
+				  <td width="22%" valign="top" class="vncellreq"><?=gettext("Trigger Level"); ?></td>
+				  <td width="78%" class="vtable">
 			<select name='trigger' class='formfldselect trigger_level_selector' id='trigger'>
 			<?php
 				foreach ($categories as $category => $categoryd) {
-				        echo "<option value=\"$category\"";
-				        if ($category == $pconfig['trigger']) echo " selected=\"selected\"";
+						echo "<option value=\"$category\"";
+						if ($category == $pconfig['trigger']) echo " selected=\"selected\"";
 					echo ">" . htmlspecialchars($categoryd) . "</option>\n";
 				}
 			?>
 			</select>
-                    <br /> <span class="vexpl"><?=gettext("When to trigger exclusion of a member"); ?></span></td>
-                </tr>
+					<br /><span class="vexpl"><?=gettext("When to trigger exclusion of a member"); ?></span></td>
+				</tr>
 		<tr>
-                  <td width="22%" valign="top" class="vncell"><?=gettext("Description"); ?></td>
-                  <td width="78%" class="vtable"> 
-                    <input name="descr" type="text" class="formfld unknown" id="descr" size="40" value="<?=htmlspecialchars($pconfig['descr']);?>" />
-                    <br /> <span class="vexpl"><?=gettext("You may enter a description here for your reference (not parsed)."); ?></span></td>
-                </tr>
-                <tr>
-                  <td width="22%" valign="top">&nbsp;</td>
-                  <td width="78%"> 
-                    <input name="Submit" type="submit" class="formbtn" value="<?=gettext("Save");?>" />
-                    <input type="button" class="formbtn" value="<?=gettext("Cancel");?>" onclick="window.location.href='<?=$referer;?>'" />
-                    <?php if (isset($id) && $a_gateway_groups[$id]): ?>
-                    <input name="id" type="hidden" value="<?=htmlspecialchars($id);?>" />
-                    <?php endif; ?>
-                  </td>
-                </tr>
-              </table>
+				  <td width="22%" valign="top" class="vncell"><?=gettext("Description"); ?></td>
+				  <td width="78%" class="vtable">
+					<input name="descr" type="text" class="formfld unknown" id="descr" size="40" value="<?=htmlspecialchars($pconfig['descr'])?>" />
+					<br /><span class="vexpl"><?=gettext("You may enter a description here for your reference (not parsed)."); ?></span></td>
+				</tr>
+				<tr>
+				  <td width="22%" valign="top">&nbsp;</td>
+				  <td width="78%">
+					<input name="Submit" type="submit" class="formbtn" value="<?=gettext("Save")?>" />
+					<input type="button" class="formbtn" value="<?=gettext("Cancel")?>" onclick="window.location.href='<?=$referer?>'" />
+					<?php if (isset($id) && $a_gateway_groups[$id]): ?>
+					<input name="id" type="hidden" value="<?=htmlspecialchars($id)?>" />
+					<?php endif; ?>
+				  </td>
+				</tr>
+			  </table>
 </form>
-<?php include("fend.inc"); ?>
-</body>
-</html>
+<?php include("foot.inc");
